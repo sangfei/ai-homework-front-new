5f176fe824a0d9c2702569f8b5646ef1dc650901
feisang@126.com <feisang@126.com>
2025-07-17 00:48:05 +0800

    add tls support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src'
+    }
+  },
+  server: {
+    https: {
+      cert: fs.readFileSync(path.resolve(__dirname, './ssl/zhifei.site.pem')),
+      key: fs.readFileSync(path.resolve(__dirname, './ssl/zhifei.site.key'))
     }
   }
 })
